b5cb07bcb7e6c9ecf6029f67c1ddd6371c2cf0b5
tilakujudebug <tilakujudebug@gmail.com>
2023-02-09 11:53:45 +0530

    first codeigniter4 project

---
+++ app/Controllers/Welcome.php
@@ -0,0 +1,16 @@
+<?php namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class Welcome extends BaseController
+{
+    public function index()
+    {
+        echo "Welcome";
+    }
+
+    public function methodName()
+    {
+        // your code for another method
+    }
+}
